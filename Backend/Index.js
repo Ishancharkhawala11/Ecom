64800a5db090ecfcbbf5dc9ce0906cf074fe49bd
Ishancharkhawala11 <ishankcharkhawala@gmail.com>
2025-03-05 00:39:07 +0530

    cache

--- Backend/Index.js
+++ Backend/Index.js
@@ -32,15 +32,16 @@ app.use(
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
     credentials: true,
   })
 );
 
+// Handle Preflight Requests (OPTIONS)
 app.options("*", (req, res) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cache-Control");
   res.header("Access-Control-Allow-Credentials", "true");
   res.sendStatus(204);
 });
