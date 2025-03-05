6ea969fc56c9f40e19a6eb7f30f0d52f7d8b648b
Ishancharkhawala11 <ishankcharkhawala@gmail.com>
2025-03-05 16:20:51 +0530

    cache

--- Backend/Index.js
+++ Backend/Index.js
@@ -40,7 +40,7 @@ app.use(
     },
     credentials: true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control"
   })
 );
 
