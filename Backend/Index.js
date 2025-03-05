eb2aa2a988d6d4dc97159acbc61154a30830a925
Ishancharkhawala11 <ishankcharkhawala@gmail.com>
2025-03-05 16:27:25 +0530

    check-auth-1

--- Backend/Index.js
+++ Backend/Index.js
@@ -16,7 +16,6 @@ const allowedOrigins = [
   "https://ecom-ishans-projects-75140e8b.vercel.app",
   "https://ecom-git-main-ishans-projects-75140e8b.vercel.app",
   "http://localhost:5173",
-  
 ];
 
 // Middleware to log incoming requests
@@ -39,7 +38,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control"
   })
 );
