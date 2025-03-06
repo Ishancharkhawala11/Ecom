70fad923948e800f0ae60691a2c572e089476b4c
Ishancharkhawala11 <ishankcharkhawala@gmail.com>
2025-03-06 12:49:19 +0530

    vit.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
-// https://vite.dev/config/
+import path from 'path'
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    historyApiFallback: true, // Ensures proper routing for React Router
+  },
+  build: {
+    outDir: 'dist',
   },
 })
